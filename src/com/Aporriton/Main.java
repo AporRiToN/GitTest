930d21619eb9a007cc196c5e394f3b4ffdda24a4
AporRitoN <xarkaltsidis@gmail.com>
2020-04-13 21:26:42 +0300

    added print message

--- src/com/Aporriton/Main.java
+++ src/com/Aporriton/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(" Making changes");
 
+
+
+
         System.out.println(" OUaOU");
         System.out.println(" MAouaaa");
 
